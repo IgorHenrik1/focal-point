67056b91b843ff03e40fbf0bab2c24c6e7b798fa
Igor <igor.freitas@gruponsc.com.br>
2024-09-22 19:28:40 -0300

    feat: adiciona componente ToDo no arquivo page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 
 import Header from "@/components/header";
+import ToDo from "@/components/to-do";
 
 export default function Home() {
   return (
     <>
       <Header />
+      <ToDo />
     </>
   );
 }
