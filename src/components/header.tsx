c5457d6bd265ec30e6b9f471c0a270e700f768a6
Igor <igor.freitas@gruponsc.com.br>
2024-09-22 19:27:55 -0300

    style(header): altera espaçamento entre os itens

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,15 +1,33 @@
+'use client'
 import Image from "next/image";
-import '@/components/header.scss'
+import '@/components/header.scss';
+import { useEffect, useState } from "react";
 
-export default function Header(){
+export default function Header() {
+  const [dataAtual, setDataAtual] = useState('');
+
+  useEffect(() => {
+    const hoje = new Date();
+    const opcoes: Intl.DateTimeFormatOptions = { 
+      weekday: 'long', 
+      year: 'numeric', 
+      month: 'long', 
+      day: 'numeric' 
+    };
+    const dataFormatada = hoje.toLocaleDateString('pt-BR', opcoes);
+    const diaComLetraMaiuscula = dataFormatada.charAt(0).toUpperCase() + dataFormatada.slice(1);
+    setDataAtual(diaComLetraMaiuscula);
+  }, []);
+
   return (
     <header>
       <div className="container">
-        <Image src="/images/logo.webp" alt="Logo Focal Point" width={150} height={36}/>
+        <div>
+          <Image src="/images/logo.webp" alt="Logo Focal Point" width={150} height={36} />
+        </div>
         <h2>Bem-vindo de volta, Marcus</h2>
-        <p>Segunda, 01 de dezembro de 2025</p>
+        <p>{dataAtual}</p>
       </div>
-
     </header>
-  )
+  );
 }
